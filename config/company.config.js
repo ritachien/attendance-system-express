2ef9f3cfb71d470958092c9b2e1f073a23e595be
Rita Chien <ritachien929@gmail.com>
2022-12-28 18:57:24 +0800

    feat: delete status table and related seeder

--- config/company.config.js
+++ config/company.config.js
@@ -1,5 +1,13 @@
+// account would be locked by continuously password error
 const loginErrorLimit = 5
+
+// statuses of records
+const attendanceStatus = {
+  error: 0,
+  ok: 1,
+}
 
 module.exports = {
   loginErrorLimit,
+  attendanceStatus,
 }
