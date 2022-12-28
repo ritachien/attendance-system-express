0a5723ff6a6c6df0f3563fcb9c15f9ff262b8bb3
Rita Chien <ritachien929@gmail.com>
2022-12-28 21:18:44 +0800

    feat: edit record table structure

--- models/record.js
+++ models/record.js
@@ -5,12 +5,15 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Record extends Model {
     static associate (models) {
-      Record.belongsTo(models.Status, { foreignKey: 'statusId' })
       Record.belongsTo(models.User, { foreignKey: 'userId' })
     }
   }
   Record.init({
+    date: DataTypes.DATEONLY,
+    clockIn: DataTypes.DATE,
+    clockOut: DataTypes.DATE,
     duration: DataTypes.INTEGER,
+    status: DataTypes.TINYINT,
   }, {
     sequelize,
     modelName: 'Record',
