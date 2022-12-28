7f5df879609b1a81d45b1ab464c293923e078a50
Rita Chien <ritachien929@gmail.com>
2022-12-28 21:21:41 +0800

    feat: add more seed users

--- seeders/20221219154700-user-seed-file.js
+++ seeders/20221219154700-user-seed-file.js
@@ -1,11 +1,19 @@
 'use strict'
-// const { v4: UUIDV4 } = require('uuid')
 const bcrypt = require('bcryptjs')
 const { randomUUID } = require('crypto')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    const users = Array.from({ length: 10 }).map((_, index) => {
+      return {
+        account: `user${index + 1}`,
+        password: bcrypt.hashSync('titaner'),
+        email: `user${index + 1}@example.com`,
+        name: `user${index + 1}`,
+        is_admin: false,
+      }
+    })
     await queryInterface.bulkInsert(
       'Users',
       [
@@ -16,13 +24,7 @@ module.exports = {
           name: 'admin',
           is_admin: true,
         },
-        {
-          account: 'user1',
-          password: bcrypt.hashSync('titaner'),
-          email: 'user1@example.com',
-          name: 'user1',
-          is_admin: false,
-        },
+        ...users,
       ].map(user => {
         return {
           ...user,
