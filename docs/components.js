a37032217707ab4c6711bae7592a2bcc91927d52
Rita Chien <ritachien929@gmail.com>
2023-01-06 00:11:45 +0800

    docs: change auth methods from Bearer JWT to header[Authorization]

--- docs/components.js
+++ docs/components.js
@@ -6,6 +6,8 @@ module.exports = {
       type: 'apiKey',
       in: 'header',
       name: 'Authorization',
+      description: '請在 TOKEN 前面加上"Bearer "，範例: Bearer YOUR_TOKEN',
+      placeholder: '請在 TOKEN 前面加上"Bearer "，範例: Bearer YOUR_TOKEN',
     },
   },
   schemas: {
