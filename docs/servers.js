dcffad8ddcf6a5a10b0ec70aa80cfb3ad048561c
Rita Chien <ritachien929@gmail.com>
2023-01-05 22:21:20 +0800

    docs: add production server into swagger

--- docs/servers.js
+++ docs/servers.js
@@ -4,5 +4,9 @@ module.exports = {
       url: 'http://localhost:3000/api/',
       description: 'Development',
     },
+    {
+      url: 'https://attendance-system-express-production.up.railway.app/api/',
+      description: 'Production',
+    },
   ],
 }
