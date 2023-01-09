3f7059f102ee6c28c03ff4237ac103588d893fc9
Rita Chien <ritachien929@gmail.com>
2023-01-09 10:51:10 +0800

    use moment instead of dayjs to fix deploy node version error

--- utils/dateHelpers.js
+++ utils/dateHelpers.js
@@ -1,36 +1,29 @@
-const dayjs = require('dayjs')
-const isToday = require('dayjs/plugin/isToday')
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
-dayjs.extend(utc)
-dayjs.extend(timezone)
-dayjs.extend(isToday)
+const moment = require('moment')
 
 const { changeDayUntil, year } = require('../config/company.config')
 const calendar = require(`../config/govCalendar/${year}.json`)
 
 function formatInTaipeiTime (date) {
-  return dayjs(date).tz('Asia/Taipei')
+  return moment(date).tz('Asia/Taipei')
 }
 
 function getRecordDate (date) {
-  // get hour of local time
-  const hour = dayjs(date).hour()
+  const day = moment(date).tz('Asia/Taipei')
+  const hour = day.hour()
 
   if (hour < changeDayUntil) {
-    return dayjs(date).subtract(1, 'day').format('YYYY-MM-DD')
+    return day.subtract(1, 'day').format('YYYY-MM-DD')
   }
-  return dayjs(date).format('YYYY-MM-DD')
+  return day.format('YYYY-MM-DD')
 }
 
 function getDuration (start, end) {
-  const ms = dayjs(end) - dayjs(start)
+  const ms = moment(end) - moment(start)
   return Math.floor(ms / 1000 / 60 / 60)
 }
 
 function isHoliday (date) {
-  const index = dayjs(date).format('YYYYMMDD')
-  console.log(date, index)
+  const index = moment(date).tz('Asia/Taipei').format('YYYYMMDD')
   return calendar[index].isHoliday
 }
 
